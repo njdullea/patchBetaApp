b70ff5af71e3e3a9713f71340cab4c757b7d4a5a
Nathan Dullea <njdullea@gmail.com>
2018-12-11 17:09:10 -0600

    added patHomePage, resHomePage

--- patchBetaApp/patHomePage.swift
+++ patchBetaApp/patHomePage.swift
@@ -7,3 +7,30 @@
 //
 
 import Foundation
+import UIKit
+import AWSDynamoDB
+import AWSMobileClient
+
+class patHomePage: UIViewController {
+    
+    override func viewDidLoad() {
+        
+        if shared_BLE_Manager.bleOn == true {
+            print("ble is on (homepage)")
+        }
+        
+        super.viewDidLoad()
+        
+        //NotificationCenter.default.addObserver(self, selector: #selector(updatePillCounter), name: Notification.Name("pillTaken"), object: nil)
+        
+    }
+    
+    //TESTING IF THIS WORKS PROPERLY BY CHECKING 'usersName' IN EXT ADHERENCE PAGE
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if (segue.identifier == "expAdherenceSegue") {
+            let vc = segue.destination as! extAdherencePage
+            vc.usersName = AWSIdentityManager.default().identityId!
+        }
+    }
+}
+
